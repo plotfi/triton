a1eadcbb1eefcfe76648a34af47d3c39f1599dae
Puyan Lotfi <puyan@puyan.org>
2026-01-31 18:03:45 -0800

    more cleanup

--- include/triton/Analysis/AxisInfo.h
+++ include/triton/Analysis/AxisInfo.h
@@ -35,72 +35,12 @@ public:
     assert(constancy.size() == contiguity.size());
   }
 
-  // contiguity[d] is the length of the shortest sequence of contiguous integers
-  // along dimension d.
-  //
-  // If we have an array of N elements with a contiguity value C, then the array
-  // can be divided into a list of N/C sequences of C contiguous elements.
-  // Since we have N = 2^k, C must be a power of two.
-  //
-  // For example, the 2D array
-  //
-  //   [[10, 11, 12, 13, 18, 19, 20, 21],
-  //    [20, 21, 22, 23, 28, 29, 30, 31]]
-  //
-  // has contiguity [1, 4], and
-  //
-  //   [[12, 16, 20, 24],
-  //    [13, 17, 21, 25],
-  //    [14, 18, 22, 26],
-  //    [15, 19, 23, 27],
-  //    [18, 22, 26, 30],
-  //    [19, 23, 27, 31]]
-  //
-  // has contiguity [2, 1].
   int64_t getContiguity(size_t dim) const { return contiguity[dim]; }
   const DimVectorT &getContiguity() const { return contiguity; }
 
-  // divisibility[d] is the largest power of two that divides the first element
-  // of all groups of length contiguity[d] along dimension d.
-  //
-  // For example,
-  //
-  //   [[10, 11, 12, 13, 18, 19, 20, 21],
-  //    [20, 21, 22, 23, 28, 29, 30, 31]]
-  //
-  //  has divisibility [1, 2], and
-  //
-  //    [[12, 16, 20, 24],
-  //     [13, 17, 21, 25],
-  //     [14, 18, 22, 26],
-  //     [15, 19, 23, 27]]
-  //
-  // has divisibility [4, 1].
-  //
-  // On the other hand,
-  //
-  //   [0, 1, 2, 0, 4, 5, 6, 7]
-  //
-  // has divisibility 1 because its contiguity is 1.
   int64_t getDivisibility(size_t dim) const { return divisibility[dim]; }
   const DimVectorT &getDivisibility() const { return divisibility; }
 
-  // constancy[d] is the length of the shortest sequence of repeating integers
-  // along dimension d.
-  //
-  // This is particularly useful to infer the contiguity of operations (e.g.
-  // add) involving a constant.
-  //
-  // If we have an array of N elements, with a constancy value C, then the array
-  // can be divided into a list of N/C sequences of C elements with the same
-  // value.  Since we have N = 2^k, C must be a power of two.
-  //
-  // For example
-  //
-  //   [[8, 8, 8, 8, 12, 12, 12, 12],
-  //    [16, 16, 16, 16, 20, 20, 20, 20]]
-  //
-  // has constancy [1, 4].
   int64_t getConstancy(size_t dim) const { return constancy[dim]; }
   const DimVectorT &getConstancy() const { return constancy; }
 
@@ -195,13 +135,6 @@ namespace axisinfo {
 using CallbackType = std::function<void(AxisInfoVisitorList &)>;
 } // namespace axisinfo
 
-// Module level axis info analysis based on the call graph, assuming that we do
-// not have recursive functions.
-//
-// Since each function will be called multiple times, we need to calculate the
-// axis info based on the axis info of all the callers.  In the future, we can
-// perform optimization using function cloning so that each call site will have
-// unique axis info.
 using AxisInfoMapT = DenseMap<Value, AxisInfo>;
 class ModuleAxisInfoAnalysis : public CallGraph<AxisInfoMapT> {
 public:
@@ -246,16 +179,6 @@ public:
   unsigned getContiguity(Value value);
   unsigned getAlignment(Value value);
 
-  // Overloads of the above methods but have separated elementBitWidth to
-  // calculate the contiguity. These are useful for computing axis info when
-  // lowering to hardware intrinsics that require a scalar/warp-uniform base ptr
-  // with separate per lane offsets like AMD buffer operations.
-  //
-  // As a concrete example, instead of a single tensor<128x64x!tt.ptr<f16>>
-  // value, now we have two separate values: !tt.ptr<f16> for the base pointer
-  // and tensor<128x64xi32> for the offset. For such cases, we want to compute
-  // the contiguity on the offsets but use the pointee element type bit width
-  // instead of the offset element type bit width for alignment
   unsigned getContiguity(Value offsetsValue, unsigned elementBitWidth);
   unsigned getAlignment(Value offsetsValue, unsigned elementBitWidth);
 
--- third_party/nano/include/TritonNANOGPUToLLVM/TypeConverter.h
+++ third_party/nano/include/TritonNANOGPUToLLVM/TypeConverter.h
@@ -28,13 +28,7 @@ public:
     auto ctx = type.getContext();
     auto blockType = type.getBlockType();
     auto shape = blockType.getShape();
-
-    // Determine the number of dwords based on tensor dimensions
-    // 2D tensors: group0 (4) + group1 (8) = 12 dwords
-    // 3D-5D tensors: group0 (4) + group1 (8) + group2 (4) + group3 (4) = 20
-    // dwords
     int numDwords = (shape.size() > 2) ? (4 + 8 + 4 + 4) : (4 + 8);
-
     auto types = SmallVector<Type>(numDwords, IntegerType::get(ctx, 32));
     return LLVM::LLVMStructType::getLiteral(ctx, types);
   }
